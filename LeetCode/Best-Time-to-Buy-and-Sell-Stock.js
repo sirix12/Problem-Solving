30dbbc9a15c390f4750597cfdca2c3c9ca2a8f8c
Asiri Dheeranath <asiridheeranath@gmail.com>
2026-06-21 21:13:22 +0530

    feat: add contains-duplicates solution and optimize TwoSum and Best-Time-to-Buy-and-Sell-Stock implementations

--- LeetCode/Best-Time-to-Buy-and-Sell-Stock.js
+++ LeetCode/Best-Time-to-Buy-and-Sell-Stock.js
@@ -2,15 +2,31 @@
  * @param {number[]} prices
  * @return {number}
  */
+// var maxProfit = function (prices) {
+//     let min = Infinity
+//     let maxPro = 0
+//     prices.forEach(pri => {
+//         if (pri < min) min = pri
+//         let pro = pri - min
+//         if (pro > maxPro) maxPro = pro
+//     })
+//     return maxPro
+// }
+
 var maxProfit = function (prices) {
-    let min = Infinity
-    let maxPro = 0
-    prices.forEach(pri => {
-        if (pri < min) min = pri
-        let pro = pri - min
-        if (pro > maxPro) maxPro = pro
-    })
-    return maxPro
+    let buy = Infinity
+    let profit = 0
+    for (let price of prices) {
+        if (buy > price) {
+            buy = price
+        } else {
+            let pro = price - buy
+            if (profit < pro) {
+                profit = pro
+            }
+        }
+    }
+    return profit
 }
 
 console.log(maxProfit([7, 6, 5, 1, 6, 4]))
--- LeetCode/TwoSum.js
+++ LeetCode/TwoSum.js
@@ -3,12 +3,26 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function (nums, target) {
-    for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            console.log(nums[i] + " " + nums[j])
-        }
-    }
-};
+// var twoSum = function (nums, target) {
+//     for (let i = 0; i < nums.length; i++) {
+//         for (let j = i + 1; j < nums.length; j++) {
+//             console.log(nums[i] + " " + nums[j])
+//         }
+//     }
+// };
 
-twoSum([2, 7, 11, 15], 9)
+function twoSum(nums, target) {
+    let mapped = new Map()
+    for (let i = 0; i < nums.length; i++) {
+        let complement = target - nums[i]
+        if (mapped.has(complement)) {
+            return [i, mapped.get(complement)]
+        }
+        mapped.set(nums[i], i)
+    }
+}
+
+
+
+
+console.log(twoSum([2, 7, 11, 15], 9))
