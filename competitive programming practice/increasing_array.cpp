2a2bbd1fe0bde56d5afcf1d04dcf35ce29b5308a
sirix12 <asiridheeranath@gmail.com>
2026-09-22 14:25:38 +0530

    completed increasing array

--- competitive programming practice/increasing_array.cpp
+++ competitive programming practice/increasing_array.cpp
@@ -1,7 +1,26 @@
 #include <bits/stdc++.h>
 #include <iostream>
+#include <vector>
 
 using namespace std;
+
+long long count_moves(const vector<long long> &numbers, long long n)
+{
+    long long prev = numbers[0];
+    long long count = 0;
+    for (int i = 1; i < n; i++)
+    {
+        if (prev > numbers[i])
+        {
+            count += prev - numbers[i];
+        }
+        else
+        {
+            prev = numbers[i];
+        }
+    }
+    return count;
+}
 
 int main()
 {
@@ -10,9 +29,12 @@ int main()
     int n;
     cin >> n;
 
-    int numbers[n];
+    vector<long long> numbers(n);
     for (int i = 0; i < n; i++)
     {
         cin >> numbers[i];
     }
+
+    cout << count_moves(numbers, n) << "\n";
+    return 0;
 }
