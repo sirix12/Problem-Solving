39d34f69cea4d48ce698cd899decec550ae9e98f
sirix12 <asiridheeranath@gmail.com>
2026-09-22 07:17:13 +0530

    started increasing_array

---
+++ competitive programming practice/increasing_array.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n;
+    cin >> n;
+
+    int numbers[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> numbers[i];
+    }
+}
